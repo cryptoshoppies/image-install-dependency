15817301aa3a35b839b2b22de147d3c742d74a63
dorofiykolya <dorofiy.kolya@gmail.com>
2023-10-17 19:26:49 +0100

    logs

--- install.sh
+++ install.sh
@@ -39,17 +39,37 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 #-------------------------------------------------------------------------------------------------------
+function echo_red {
+    echo -e "\033[0;31m$1\033[0m"
+}
+function echo_green {
+    echo -e "\033[0;32m$1\033[0m"
+}
+function echo_yellow {
+    echo -e "\033[0;33m$1\033[0m"
+}
+function echo_blue {
+    echo -e "\033[0;34m$1\033[0m"
+}
+function echo_line {
+    echo_blue "----------------------------------------"
+}
+#-------------------------------------------------------------------------------------------------------
 
-echo "----------------------------------------"
-echo "VERSION=$VERSION"
-echo "ARCH=$ARCH"
-echo "SOURCE_BY=$SOURCE_BY"
-echo "----------------------------------------"
+echo_line
+echo_blue "VERSION=$VERSION"
+echo_blue "ARCH=$ARCH"
+echo_blue "SOURCE_BY=$SOURCE_BY"
+echo_line
 
 #-------------------------------------------------------------------------------------------------------
 set -e
-apt update
-apt install zip -y
+
+echo_line
+    apt update
+echo_line
+    apt install zip -y
+echo_line
 
 if [[ "$SOURCE_BY" == "tag" ]]; then
     curl https://codeload.github.com/NeuralInnovations/runner-images/zip/refs/tags/$VERSION -o ./images.zip
@@ -57,12 +77,21 @@ else
     curl https://github.com/NeuralInnovations/runner-images/archive/refs/heads/$VERSION.zip -o ./images.zip
 fi
 
-unzip -o ./images.zip -d ./
+echo_line
+    unzip -o ./images.zip -d ./
+echo_line
 cd ./runner-images-$VERSION
 chmod -R 777 ./images/linux/
-./images/linux/install.sh --arch $ARCH
+echo_line
+    ./images/linux/install.sh --arch $ARCH
+echo_line
 cd ..
-rm ./images.zip
-rm ./image-install.sh
-rm -fr ./runner-images-$VERSION
+echo_line
+    rm ./images.zip
+echo_line
+    rm ./image-install.sh
+echo_line
+    rm -fr ./runner-images-$VERSION
+echo_line
+echo_green "DONE"
 #-------------------------------------------------------------------------------------------------------
